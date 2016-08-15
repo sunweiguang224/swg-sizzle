72ff93b30e196eb7e8bb0c4864bddbff68eab6c7
Sun Weiguang <sunweiguang224@gmail.com>
2016-08-15 16:17:06 +0800

    npm sizzle

---
+++ test/karma/launchers.js
@@ -0,0 +1,259 @@
+"use strict";
+
+module.exports = {
+	"bs_firefox-3.6": {
+		base: "BrowserStack",
+		browser: "firefox",
+		browser_version: "3.6",
+		os: "OS X",
+		os_version: "Mavericks"
+	},
+	"bs_firefox-38": {
+		base: "BrowserStack",
+		browser: "firefox",
+		browser_version: "38.0",
+		os: "OS X",
+		os_version: "Mavericks"
+	},
+	"bs_firefox-45": {
+		base: "BrowserStack",
+		browser: "firefox",
+		browser_version: "45.0",
+		os: "OS X",
+		os_version: "Mavericks"
+	},
+	"bs_firefox-46": {
+		base: "BrowserStack",
+		browser: "firefox",
+		browser_version: "46.0",
+		os: "OS X",
+		os_version: "El Capitan"
+	},
+	"bs_firefox-47": {
+		base: "BrowserStack",
+		browser: "firefox",
+		browser_version: "47.0",
+		os: "OS X",
+		os_version: "El Capitan"
+	},
+
+	"bs_chrome-16": {
+		base: "BrowserStack",
+		browser: "chrome",
+		browser_version: "16.0",
+		os: "OS X",
+		os_version: "Mavericks"
+	},
+	"bs_chrome-24": {
+		base: "BrowserStack",
+		browser: "chrome",
+		browser_version: "24.0",
+		os: "OS X",
+		os_version: "Mavericks"
+	},
+	"bs_chrome-50": {
+		base: "BrowserStack",
+		browser: "chrome",
+		browser_version: "50.0",
+		os: "OS X",
+		os_version: "El Capitan"
+	},
+	"bs_chrome-51": {
+		base: "BrowserStack",
+		browser: "chrome",
+		browser_version: "51.0",
+		os: "OS X",
+		os_version: "El Capitan"
+	},
+
+	"bs_edge-13": {
+		base: "BrowserStack",
+		browser: "edge",
+		browser_version: "13.0",
+		os: "Windows",
+		os_version: "10"
+	},
+
+	"bs_ie-6": {
+		base: "BrowserStack",
+		browser: "ie",
+		browser_version: "6.0",
+		os: "Windows",
+		os_version: "XP"
+	},
+	"bs_ie-7": {
+		base: "BrowserStack",
+		browser: "ie",
+		browser_version: "7.0",
+		os: "Windows",
+		os_version: "XP"
+	},
+	"bs_ie-8": {
+		base: "BrowserStack",
+		browser: "ie",
+		browser_version: "8.0",
+		os: "Windows",
+		os_version: "7"
+	},
+	"bs_ie-9": {
+		base: "BrowserStack",
+		browser: "ie",
+		browser_version: "9.0",
+		os: "Windows",
+		os_version: "7"
+	},
+	"bs_ie-10": {
+		base: "BrowserStack",
+		browser: "ie",
+		browser_version: "10.0",
+		os: "Windows",
+		os_version: "8"
+	},
+	"bs_ie-11": {
+		base: "BrowserStack",
+		browser: "ie",
+		browser_version: "11.0",
+		os: "Windows",
+		os_version: "8.1"
+	},
+
+	"bs_opera-11.6": {
+		base: "BrowserStack",
+		browser: "opera",
+		browser_version: "11.6",
+		os: "Windows",
+		os_version: "7"
+	},
+	"bs_opera-12.16": {
+		base: "BrowserStack",
+		browser: "opera",
+		browser_version: "12.16",
+		os: "Windows",
+		os_version: "7"
+	},
+	"bs_opera-37": {
+		base: "BrowserStack",
+		browser: "opera",
+		browser_version: "37.0",
+		os: "OS X",
+		os_version: "El Capitan"
+	},
+	"bs_opera-38": {
+		base: "BrowserStack",
+		browser: "opera",
+		browser_version: "38.0",
+		os: "OS X",
+		os_version: "El Capitan"
+	},
+
+	"bs_safari-4.0": {
+		base: "BrowserStack",
+		browser: "safari",
+		browser_version: "4.0",
+		os: "OS X",
+		os_version: "Snow Leopard"
+	},
+	"bs_safari-5.0": {
+		base: "BrowserStack",
+		browser: "safari",
+		browser_version: "5.0",
+		os: "OS X",
+		os_version: "Snow Leopard"
+	},
+	"bs_safari-5.1": {
+		base: "BrowserStack",
+		browser: "safari",
+		browser_version: "5.1",
+		os: "OS X",
+		os_version: "Lion"
+	},
+	"bs_safari-6.0": {
+		base: "BrowserStack",
+		browser: "safari",
+		browser_version: "6.0",
+		os: "OS X",
+		os_version: "Lion"
+	},
+	"bs_safari-8.0": {
+		base: "BrowserStack",
+		browser: "safari",
+		browser_version: "8.0",
+		os: "OS X",
+		os_version: "Yosemite"
+	},
+	"bs_safari-9.1": {
+		base: "BrowserStack",
+		browser: "safari",
+		browser_version: "9.1",
+		os: "OS X",
+		os_version: "El Capitan"
+	},
+
+	"bs_ios-5.1": {
+		base: "BrowserStack",
+		device: "iPhone 4S",
+		os: "ios",
+		os_version: "5.1"
+	},
+	"bs_ios-6.0": {
+		base: "BrowserStack",
+		device: "iPhone 5",
+		os: "ios",
+		os_version: "6.0"
+	},
+	"bs_ios-7.0": {
+		base: "BrowserStack",
+		device: "iPhone 5S",
+		os: "ios",
+		os_version: "7.0"
+	},
+	"bs_ios-8.3": {
+		base: "BrowserStack",
+		device: "iPhone 6",
+		os: "ios",
+		os_version: "8.3"
+	},
+	"bs_ios-9.3": {
+		base: "BrowserStack",
+		device: "iPhone 6S",
+		os: "ios",
+		os_version: "9.3"
+	},
+
+	"bs_android-2.3": {
+		base: "BrowserStack",
+		device: "Motorola Droid Razr",
+		os: "android",
+		os_version: "2.3"
+	},
+	"bs_android-4.0": {
+		base: "BrowserStack",
+		device: "Motorola Razr",
+		os: "android",
+		os_version: "4.0"
+	},
+	"bs_android-4.1": {
+		base: "BrowserStack",
+		device: "Google Nexus 7",
+		os: "android",
+		os_version: "4.1"
+	},
+	"bs_android-4.2": {
+		base: "BrowserStack",
+		device: "LG Nexus 4",
+		os: "android",
+		os_version: "4.2"
+	},
+	"bs_android-4.3": {
+		base: "BrowserStack",
+		device: "Samsung Galaxy S4",
+		os: "android",
+		os_version: "4.3"
+	},
+	"bs_android-4.4": {
+		base: "BrowserStack",
+		device: "Samsung Galaxy S5",
+		os: "android",
+		os_version: "4.4"
+	}
+};
